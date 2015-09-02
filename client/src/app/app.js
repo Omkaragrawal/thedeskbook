91d53db89167e4019f97731fea7563271f90e571
Rahil <rahil.shaikh@accionlabs.com>
2015-09-02 18:22:50 +0530

    adding Ui.router

--- client/src/app/app.js
+++ client/src/app/app.js
@@ -5,3 +5,21 @@ var app = angular.module('theDeskBook', ['ui.router']);
 app.controller('MainController', function($scope) {
     $scope.message = 'Angular Works!';
 });
+
+app.config(function($stateProvider, $urlRouterProvider) {
+	
+	$urlRouterProvider.otherwise("/");
+	
+	$stateProvider
+	.state('landing', {
+		url: "/",
+		views : {
+			"" : {
+				templateUrl:"app/components/landing/landing.html"
+			},
+			"header@landing":{
+				template:"YOOOO"
+			}
+		}
+	});
+});
